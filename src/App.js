1bdef2d115afac2071a41834165678f4b2044c47
murpan <qw04011@gmail.com>
2021-11-30 21:36:08 +0900

    [#4] feat : Rotues 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+// eslint-disable-next-line sort-imports
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SideNavbar from 'components/SideNavbar/SideNavbar';
+import chatPage from 'pages/chatPage';
+import historyPage from 'pages/historyPage';
+import keywordPage from 'pages/keywordPage';
+import myPage from 'pages/myPage';
 
 const App = () => (
   <div className="Main">
-    <SideNavbar />
+    <BrowserRouter>
+      <SideNavbar />
+      <Routes>
+        <Route path="/history" component={historyPage} exact />
+        <Route path="/chat" component={chatPage} exact />
+        <Route path="/keyword" component={keywordPage} exact />
+        <Route path="/mypage" component={myPage} exact />
+      </Routes>
+    </BrowserRouter>
   </div>
 );
 
