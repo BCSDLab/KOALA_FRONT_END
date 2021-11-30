2acd64da69358fc9b121bc4d9de431f89a9c2b59
murpan <qw04011@gmail.com>
2021-11-30 21:06:35 +0900

    [#4] style : SideNavbar 스타일 적용

--- src/components/SideNavbar/SideNavbar.js
+++ src/components/SideNavbar/SideNavbar.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
@@ -7,16 +8,51 @@ const Nav = styled.div`
   padding: 40px 24px 91px;
   box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.15);
   background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ImgBlock = styled.div`
+  width: 32px;
+  height: 32px;
+  margin: 20px 0;
+  object-fit: contain;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const OpenMenu = styled.div`
+  width: 26px;
+  height: 20px;
+  margin: 0 3px 80px;
+  object-fit: contain;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const SideNavbar = () => {
-  const [sidebar, setSidebar] = useState(false);
-
-  const showSidebar = () => setSidebar(!sidebar);
-
   return (
     <div>
-      <Nav>info</Nav>
+      <Nav>
+        <OpenMenu>
+          <img src="/asset/Vector.png" alt="Vector" />
+        </OpenMenu>
+        <ImgBlock>
+          <img src="/asset/hashtag.png" alt="keyword" />
+        </ImgBlock>
+        <ImgBlock>
+          <img src="/asset/archive.png" alt="history" />
+        </ImgBlock>
+        <ImgBlock>
+          <img src="/asset/chat.png" alt="chat" />
+        </ImgBlock>
+        <ImgBlock>
+          <img src="/asset/cog.png" alt="mypage" />
+        </ImgBlock>
+      </Nav>
     </div>
   );
 };
