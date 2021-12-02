bd0504a22087a16cf4da8c236a1c213dbf1b02d7
murpan <qw04011@gmail.com>
2021-12-02 14:53:52 +0900

    Revert "[#4] style : AuthTemplate 스타일 작성"
    This reverts commit 7ae0c4d746837450d8f115c611648cafba5cc0dc.

--- src/components/Login/AuthTemplate.js
+++ src/components/Login/AuthTemplate.js
@@ -1,33 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const AuthTemplateBlock = styled.div`
-  padding: 0 776px 0 776px;
-  display: flex;
-  justify-content: center;
-  alignitems: center;
-  width: 368px;
-`;
-
-const Box = styled.div`
-  width: 368px;
-  padding-top: 280px;
-`;
-
-const MainLogo = styled.img`
-    width:122.8px
-    height: 34px;
-  padding: 0 120px 0 125.2px;
-`;
+const AuthTemplateBlock = styled.div``;
 
 // eslint-disable-next-line react/prop-types
-const AuthTemplate = ({ children }) => (
-  <AuthTemplateBlock>
-    <Box>
-      <MainLogo src="/asset/Group 1581.png" alt="logo" />
-      {children}
-    </Box>
-  </AuthTemplateBlock>
-);
+const AuthTemplate = ({ children }) => <AuthTemplateBlock>{children}</AuthTemplateBlock>;
 
 export default AuthTemplate;
