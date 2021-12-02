2341fa1716cbdc2f66fe570e20f036ae2d6ac42e
murpan <qw04011@gmail.com>
2021-12-02 17:49:09 +0900

    [#5] feat : 공통컴포넌트 Button 생성

---
+++ src/components/Shared/Button.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  width: 368px;
+  height: 48px;
+  flex-grow: 0;
+  color: white;
+  margin-top: 32px;
+  ouline: none;
+  background: black;
+  &:hover {
+    background: gray;
+  }
+`;
+
+// eslint-disable-next-line react/jsx-props-no-spreading
+const Button = (props) => <StyledButton {...props} />;
+
+export default Button;
