7dff63cd3941b9eb81179e098219673165f3ca36
murpan <qw04011@gmail.com>
2021-12-03 20:51:38 +0900

    [#5] feat : Route 설정 변경

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
-// eslint-disable-next-line sort-imports
 import './App.css';
 import { Route, Routes } from 'react-router';
-// eslint-disable-next-line sort-imports
 import LoginPage from 'pages/LoginPage';
 import RegisterPage from 'pages/RegisterPage';
 
 function App() {
   return (
-    <div>
-      <LoginPage />
-      <Routes>
-        <Route component={LoginPage} path="/login" exact />
-        <Route component={RegisterPage} path="/register" exact />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
   );
 }
 
