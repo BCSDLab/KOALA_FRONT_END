cc22161c65d77062b1f903f707d15532d2ab67d2
murpan <qw04011@gmail.com>
2021-12-03 17:14:03 +0900

    [#4] refactor : styles 파일 내 중복속성 상속

--- src/components/SideNavbar/styles.js
+++ src/components/SideNavbar/styles.js
@@ -3,17 +3,34 @@ import styled from 'styled-components';
 export const SideMenuBackground = styled.div`
   width: 270px;
   height: 860px;
-  padding: 38px 0 38px;
+  padding: 36.5px 0 38px;
   background-color: #f6f7f8;
 `;
 
 export const SideMenuContainer = styled.div`
-  margin: 0px 0px 78.2px 24px;
+  margin: 0px 0px 69px 18px;
 `;
 
 export const Logo = styled.img`
   width: 80px;
   height: 21.8px;
+`;
+
+const List = styled.div`
+  width: 270px;
+  height: 76px;
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  text-align: left;
+`;
+
+const Link = styled.div`
+  width: 270px;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  text-align: left;
 `;
 
 export const KeywordMain = styled.div`
@@ -35,8 +52,8 @@ export const KeywordDropdown = styled.img`
   width: 24px;
   height: 24px;
   margin: 2.2px 169px 24px 6px;
-  object-fit: contain;
 `;
+
 export const KeywordElement = styled.div`
   width: 270px;
   height: 220px;
@@ -47,25 +64,14 @@ export const KeywordElement = styled.div`
   text-align: left;
 `;
 
-export const KeywordDropmenu = styled.div`
-  width: 270px;
-  height: 44px;
-  display: flex;
-  align-items: center;
+export const KeywordDropmenu = styled(Link)`
   font-size: 14px;
-  text-align: left;
 `;
 
 export const KeywordText = styled.div`
   width: 190px;
   height: 21px;
   padding: 12px 0 11.5px 32px;
-`;
-
-export const AddKeyword = styled.div`
-  width: 270px;
-  height: 65.5px;
-  display: flex;
 `;
 
 export const KeywordUpdate = styled.div`
@@ -84,33 +90,25 @@ export const AddImg = styled.img`
   margin: 18px 8px 15.5px 32px;
 `;
 
+export const AddKeyword = styled(List)``;
+
 export const AddText = styled.div`
-width: 94px;
-height: 21px;
-margin: 22px 112px 15.5px 8px;
-font-size: 11px;
-text-align: left;
-color: #222;
-}
+  width: 94px;
+  height: 21px;
+  margin: 22px 0 15.5px 8px;
+  font-size: 12px;
+  text-align: left;
+  color: #222;
 `;
 
-export const HistoryList = styled.div`
-  width: 270px;
-  height: 76px;
+export const HistoryList = styled(List)`
   border-top: 1px solid #eee;
   border-bottom: 1px solid #eee;
-  align-items: center;
-  font-size: 14px;
-  text-align: left;
 `;
 
-export const HistoryLink = styled.div`
-  width: 270px;
-  height: 76px;
+export const HistoryLink = styled(Link)`
   margin: 27.5px 0;
-  align-items: center;
   font-size: 14px;
-  text-align: left;
 `;
 export const HistoryText = styled.span`
   width: 52px;
@@ -121,22 +119,10 @@ export const HistoryText = styled.span`
   color: #222;
 `;
 
-export const ChatList = styled.div`
-  width: 270px;
-  height: 76px;
-  display: flex;
-  align-items: center;
-  font-size: 14px;
-  text-align: left;
-`;
+export const ChatList = styled(List)``;
 
 export const ChatLink = styled.div`
-  width: 270px;
-  height: 44px;
-  display: flex;
-  align-items: center;
   font-size: 12px;
-  text-align: left;
 `;
 
 export const ChatText = styled.div`
@@ -154,33 +140,22 @@ export const SetList = styled.div`
   font-size: 14px;
   text-align: left;
 `;
-export const Set = styled.div`
-  width: 270px;
-  height: 44px;
+
+export const Set = styled(Link)`
   padding: 0 0 4px 0;
-  display: flex;
-  align-items: center;
   font-size: 14px;
-  text-align: left;
 `;
 
 export const SetText = styled.div`
-  width: 30px;
   height: 24px;
   padding: 12px 208px 8px 12px;
 `;
 
-export const Contact = styled.div`
-  width: 270px;
-  height: 44px;
-  display: flex;
-  align-items: center;
+export const Contact = styled(Link)`
   font-size: 14px;
-  text-align: left;
 `;
 
 export const ContactText = styled.div`
-  width: 59px;
   height: 24px;
   padding: 32px 179px 12px 12px;
 `;
