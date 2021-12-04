d0612a2537ede2db2352145e785752af5640365c
murpan <qw04011@gmail.com>
2021-12-04 21:16:08 +0900

    [#5] style : 비밀번호 찾기 스타일 작성

--- src/components/Auth/PwdFindForm.js
+++ src/components/Auth/PwdFindForm.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import * as S from 'components/Auth/styles';
+import Button from 'components/Shared/Button';
+import AuthNumber from 'components/Shared/AuthNumber';
 
 const PwdFindForm = () => {
-  return <div>pwdfind</div>;
+  return (
+    <div>
+      <S.Title>비밀번호</S.Title>
+      <S.StyledInput placeholder="아이디 입력"></S.StyledInput>
+      <S.StyledInput placeholder="이메일 입력"></S.StyledInput>
+      <AuthNumber></AuthNumber>
+      <Button>인증하기</Button>
+    </div>
+  );
 };
 
 export default PwdFindForm;
