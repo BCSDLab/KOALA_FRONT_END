afe5e405d511e2ec9a456039ca28bdec2062afc5
murpan <qw04011@gmail.com>
2021-12-04 20:59:32 +0900

    [#5] style : 아이디 찾기 스타일 작성

--- src/components/Auth/IdFindForm.js
+++ src/components/Auth/IdFindForm.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import AuthNumber from 'components/Shared/AuthNumber';
+import Button from 'components/Shared/Button';
+import * as S from 'components/Auth/styles';
+import styled from 'styled-components';
+
+const IdfForm = styled.div`
+  margin-bottom: 120px;
+`;
 
 const IdfindForm = () => {
-  return <div>idfind</div>;
+  return (
+    <div>
+      <IdfForm>
+        <S.Title>아이디 찾기</S.Title>
+        <S.StyledInput placeholder="이메일 입력"></S.StyledInput>
+        <AuthNumber></AuthNumber>
+      </IdfForm>
+      <Button>다음</Button>
+    </div>
+  );
 };
 
 export default IdfindForm;
