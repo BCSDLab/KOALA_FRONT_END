14f974c1cc78bcbf6cdb3b856ac85382e2548eed
murpan <qw04011@gmail.com>
2021-12-06 21:20:00 +0900

    [#5] fix : 비밀번호 변경 페이지 path 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import { Routes, Route } from 'react-router-dom';
-import SideNavbar from 'components/SideNavbar';
-import ChatPage from 'pages/ChatPage';
-import HistoryPage from 'pages/HistoryPage';
-import KeywordPage from 'pages/KeywordPage';
-import MyPage from 'pages/MyPage';
+import React from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router';
 import AuthPage from 'pages/AuthPage';
 import AuthMainForm from 'components/Auth/AuthMainForm';
 import IdFindForm from 'components/Auth/IdFindForm';
@@ -11,33 +8,20 @@ import PwdFindForm from 'components/Auth/PwdFindForm';
 import RegisterDocForm from 'components/Auth/RegisterDocForm';
 import RegisterForm from 'components/Auth/RegisterForm';
 import PwdChange from 'components/Auth/PwdChange';
-import styled from 'styled-components';
-import 'App.css';
 
-const Main = styled.div`
-  width: 100%;
-  height: 100%;
-`;
-
-const App = () => (
-  <Main>
-    <SideNavbar />
+function App() {
+  return (
     <Routes>
-      <Route path="/history" element={<HistoryPage />} />
-      <Route path="/chat" element={<ChatPage />} />
-      <Route path="/keyword" element={<KeywordPage />} />
-      <Route path="/mypage" element={<MyPage />} />
       <Route path="auth/*" element={<AuthPage />}>
         <Route index element={<AuthMainForm />} />
         <Route path="register" element={<RegisterDocForm />} />
         <Route path="registerform" element={<RegisterForm />} />
         <Route path="idfind" element={<IdFindForm />} />
         <Route path="pwdfind" element={<PwdFindForm />} />
-        <Route path="pwdchge" element={<PwdChange />} />
+        <Route path="pwdchange" element={<PwdChange />} />
       </Route>
     </Routes>
-  </Main>
-);
-
+  );
+}
 
 export default App;
