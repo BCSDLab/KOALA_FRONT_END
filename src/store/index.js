87758ad72f477532484221e10601c5b8a1c2653e
murpan <qw04011@gmail.com>
2021-12-07 20:23:04 +0900

    [#8] feat : 루트리덕스에 추가된 파일 작성

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,15 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
+import { all } from 'redux-saga/effects';
+import auth, { authSaga } from './auth';
+import loading from './loading';
 
 const rootReducer = combineReducers({
+  auth,
+  loading,
+});
 
-})
+export function* rootSaga() {
+  yield all([authSaga()]);
+}
 
-export default rootReducer
+export default rootReducer;
