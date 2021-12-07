a6df7a59e2f6c3fb1ccab6a49f8dc2a09859b18c
murpan <qw04011@gmail.com>
2021-12-07 14:50:58 +0900

    [#5] feat : 비밀번호 숨기기 보이기 기능 추가

--- src/components/Auth/PwdInput.js
+++ src/components/Auth/PwdInput.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import * as S from 'components/Auth/styles';
 
+const StyledPwd = styled.div`
+  position: relative;
+`;
+
+const PwdSee = styled.span`
+  cursor: pointer;
+`;
 const EyeImg = styled.img`
   position: absolute;
   top: 31px;
@@ -9,15 +16,29 @@ const EyeImg = styled.img`
   right: 0;
 `;
 
-const StyledPwd = styled.div`
-  position: relative;
-`;
-
 const PwdInput = (props) => {
+  const [isPasswordType, setIsPassswordType] = useState({
+    type: 'password',
+    visible: false,
+  });
+
+  const handlePasswordType = (e) => {
+    setIsPassswordType(() => {
+      if (!isPasswordType.visible) return { type: 'text', visible: true };
+      return { type: 'password', visible: false };
+    });
+  };
   return (
     <StyledPwd>
-      <S.StyledInput autoComplete="new-password" name="password" placeholder={props.placeholder} type="password" />
-      <EyeImg src="/asset/pwdEye.svg" alt="eye" />
+      <S.StyledInput
+        autoComplete="new-password"
+        name="password"
+        placeholder={props.placeholder}
+        type={isPasswordType.type}
+      />
+      <PwdSee onClick={handlePasswordType}>
+        <EyeImg src="/asset/pwdEye.svg" alt="eye" />
+      </PwdSee>
     </StyledPwd>
   );
 };
