048d4ce21eec14484e40b566bbb3684fb136e702
murpan <qw04011@gmail.com>
2021-12-16 20:30:56 +0900

    [#8] chore : api/unlogined 제거

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,5 @@
 import logined from './logined';
-import unlogined from './unlogined';
 
-export const login = ({ account, password }) => unlogined.post('user/login', { account, password });
+export const login = ({ account, password }) => logined.post('user/login', { account, password });
 
 export const refresh = () => logined.post('user/refresh');
--- src/api/unlogined.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const unlogined = axios.create({});
-
-logined.defaults.baseURL = process.env.REACT_APP_BASE_URL;
-
-export default unlogined;
