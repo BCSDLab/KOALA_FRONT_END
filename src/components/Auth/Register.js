3a78e96451dc84a4bc6e63b32e95359d8f163209
daepan <qw04011@gmail.com>
2021-12-17 21:58:59 +0900

    [#10] feat : 회원가입 input 값 저장 기능 추가
    회원가입 시 input을 넣으면 값을 로컬에 상태를 저장한다.

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'components/Shared/Button';
 import * as S from 'components/Auth/styles';
 import PwdInput from 'components/Auth/PwdInput';
 
 const RegisterForm = () => {
+  const [account, setAccount] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [checkPwd, setCheckPwd] = useState('');
+  const [email, setEmail] = useState('');
+  const [nickName, setNickName] = useState('');
+
+  const submitCreate = (e) => {
+    e.preventDefault();
+    console.log({ account, pwd, checkPwd, email, nickName });
+  };
+
+  const insertAccount = (e) => {
+    setAccount(e.target.value);
+  };
+  const insertPwd = (e) => {
+    setPwd(e.target.value);
+  };
+  const insertPwdCheck = (e) => {
+    setCheckPwd(e.target.value);
+  };
+  const insertEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const insertNickName = (e) => {
+    setNickName(e.target.value);
+  };
+
   return (
-    <div>
+    <form onSubmit={submitCreate}>
       <S.Title>회원가입</S.Title>
-      <S.StyledInput placeholder="아이디" />
-      <PwdInput placeholder="비밀번호 입력" />
-      <PwdInput placeholder="비밀번호 확인" />
-      <S.StyledInput placeholder="이메일" />
-      <S.StyledInput placeholder="닉네임" />
+      <S.StyledInput name="account" value={account} onChange={insertAccount} placeholder="아이디" />
+      <PwdInput name="pwd" value={pwd} onChange={insertPwd} placeholder="비밀번호 입력" />
+      <PwdInput name="checkPwd" value={checkPwd} onChange={insertPwdCheck} placeholder="비밀번호 확인" />
+      <S.StyledInput name="email" value={email} onChange={insertEmail} placeholder="이메일" />
+      <S.StyledInput name="nickName" value={nickName} onChange={insertNickName} placeholder="닉네임" />
       <Button>다음</Button>
-    </div>
+    </form>
   );
 };
 
