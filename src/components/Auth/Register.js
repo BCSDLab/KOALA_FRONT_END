cf07585837110f975eef32e7fe1f72892e70117a
daepan <qw04011@gmail.com>
2021-12-18 13:56:05 +0900

    [#10] feat : 비밀번호 확인 기능 추가
    비밀번호의 형식 불일치 시 에러 메세지 출력 및 비밀번호 확인 기능 추가

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -12,7 +12,7 @@ const ErrorAlert = styled.span`
 
 const RegisterForm = () => {
   const [account, setAccount] = useState('');
-  const [password, setpassword] = useState('');
+  const [password, setPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm] = useState('');
   const [email, setEmail] = useState('');
   const [nickName, setNickName] = useState('');
@@ -35,12 +35,35 @@ const RegisterForm = () => {
   const insertAccount = (e) => {
     setAccount(e.target.value);
   };
-  const insertPwd = (e) => {
-    setpassword(e.target.value);
-  };
-  const insertPwdCheck = (e) => {
-    setPasswordConfirm(e.target.value);
-  };
+  const onChangePassword = useCallback((e) => {
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+    const passwordCurrent = e.target.value;
+    setPassword(passwordCurrent);
+
+    if (!passwordRegex.test(passwordCurrent)) {
+      setPasswordMessage('숫자+영문자+특수문자 조합으로 8자리 이상 입력바랍니다');
+      setIsPassword(false);
+    } else {
+      setPasswordMessage('비밀번호 확인');
+      setIsPassword(true);
+    }
+  }, []);
+
+  const onChangePasswordConfirm = useCallback(
+    (e) => {
+      const passwordConfirmCurrent = e.target.value;
+      setPasswordConfirm(passwordConfirmCurrent);
+
+      if (password === passwordConfirmCurrent) {
+        setPasswordConfirmMessage('비밀번호 일치');
+        setIsPasswordConfirm(true);
+      } else {
+        setPasswordConfirmMessage('비밀번호가 다릅니다');
+        setIsPasswordConfirm(false);
+      }
+    },
+    [password]
+  );
   const onChangeEmail = useCallback((e) => {
     const emailRegex =
       /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
@@ -51,7 +74,7 @@ const RegisterForm = () => {
       setEmailMessage('이메일 형식이 일치하지 않습니다.');
       setIsEmail(false);
     } else {
-      setEmailMessage('올바른 이메일 형식입니다.');
+      setEmailMessage('이메일 형식이 일치합니다.');
       setIsEmail(true);
     }
   }, []);
@@ -63,8 +86,15 @@ const RegisterForm = () => {
     <form onSubmit={onSubmit}>
       <S.Title>회원가입</S.Title>
       <S.StyledInput name="account" value={account} onChange={insertAccount} placeholder="아이디" />
-      <PwdInput name="password" value={password} onChange={insertPwd} placeholder="비밀번호 입력" />
-      <PwdInput name="passwordConfirm" value={passwordConfirm} onChange={insertPwdCheck} placeholder="비밀번호 확인" />
+      <PwdInput name="password" value={password} onChange={onChangePassword} placeholder="비밀번호 입력" />
+      <ErrorAlert>{passwordMessage}</ErrorAlert>
+      <PwdInput
+        name="passwordConfirm"
+        value={passwordConfirm}
+        onChange={onChangePasswordConfirm}
+        placeholder="비밀번호 확인"
+      />
+      <ErrorAlert>{passwordConfirmMessage}</ErrorAlert>
       <S.StyledInput name="email" value={email} onChange={onChangeEmail} placeholder="이메일" />
       <ErrorAlert>{emailMessage}</ErrorAlert>
       <S.StyledInput name="nickName" value={nickName} onChange={insertNickName} placeholder="닉네임" />
