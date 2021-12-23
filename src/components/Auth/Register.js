dcddb3c5803d15d3b13537352c96e6f23810b262
daepan <qw04011@gmail.com>
2021-12-23 18:36:39 +0900

    [#10] fix : import 에러 수정
    constant 폴더 이름 변경으로 인한 오류 수정

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -4,7 +4,7 @@ import * as S from 'components/Auth/styles';
 import PwdInput from 'components/Auth/PwdInput';
 import IdInput from 'components/Auth/IdInput';
 import styled from 'styled-components';
-import { ACCOUNT_ERROR, EMAIL_ERROR, NICKNAME_ERROR } from 'constant/path';
+import { ACCOUNT_ERROR, EMAIL_ERROR, NICKNAME_ERROR } from 'constant';
 import { useDispatch, useSelector } from 'react-redux';
 import { signUp } from 'store/auth';
 
