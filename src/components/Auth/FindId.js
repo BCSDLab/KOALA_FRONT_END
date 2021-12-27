26fd8210c0120cbb3049ea42ebdae431fc84057f
crazy-oung <hdayoungh@gmail.com>
2021-12-28 01:53:02 +0900

    [#11] refactor: 아이디 찾기 컴포넌트 StyledInput에서 IdInput으로 변경

--- src/components/Auth/FindId.js
+++ src/components/Auth/FindId.js
@@ -2,19 +2,13 @@ import React, { useState, useCallback, useEffect } from 'react';
 import AuthNumber from 'components/Shared/AuthNumber';
 import Button from 'components/Shared/Button';
 import * as S from 'components/Auth/styles';
+import IdInput from 'components/Auth/Shared/IdInput';
 import styled from 'styled-components';
 
 const IdfForm = styled.div`
   margin-bottom: 120px;
 `;
 
-const StyledInput = styled(S.StyledInput)`
-  border: solid 1px ${({ isError }) => (isError ? '#ffd25d' : '#c4c4c4')};
-  background-image: ${({ isError }) => (isError ? `url('/asset/inputError.svg')` : 'none')};
-  background-position-y: center;
-  background-position-x: 332px;
-  background-repeat: no-repeat;
-`;
 const NextButton = styled(Button)`
   margin-top: 0;
 `;
@@ -37,10 +31,10 @@ const FindId = () => {
   const onChangeEmail = useCallback((e) => {
     const emailRegex =
       /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-    const emailCurrent = e.target.value;
-    setEmail(emailCurrent);
+    const currentEmail = e.target.value;
+    setEmail(currentEmail);
 
-    if (!emailRegex.test(emailCurrent)) {
+    if (!emailRegex.test(currentEmail)) {
       setEmailMessage('이메일 형식이 알맞지 않습니다.');
       setIsEmail(false);
     } else {
@@ -50,27 +44,22 @@ const FindId = () => {
   }, []);
 
   useEffect(() => {
-    if (isEmail) {
-      setIsDisabled(false);
-    } else {
-      setIsDisabled(true);
-    }
-  }, [isEmail, email]);
+    setIsDisabled(!isEmail);
+  }, [isEmail]);
 
   return (
     <div>
       <form>
         <IdfForm>
           <S.Title>아이디 찾기</S.Title>
-          <StyledInput
+          <IdInput
             name="email"
             value={email}
             onChange={onChangeEmail}
             placeholder="이메일 입력 (인증번호가 전송됩니다.)"
-            isError={emailMessage != ''}
+            isError={emailMessage !== ''}
+            errorMessage={emailMessage}
           />
-          <S.InputErrorText>{emailMessage}</S.InputErrorText>
-
           <AuthNumber isEmail={isEmail} />
         </IdfForm>
 
