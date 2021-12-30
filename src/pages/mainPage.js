d18757ef3959a4e28e57ba060daabb3a7377ad62
Sangyun <tkfkd4447@naver.com>
2021-12-30 20:16:14 +0900

    [#17] style : KeywordPage 큰 뼈대 제작
    SideNavbar와 KeywordPage 컴포넌트를 flex를 통해서 정렬해주었습니다.

--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import SideNavbar from 'components/SideNavbar';
+import KeywordPage from './keywordPage';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display:flex;
+`;
 
 const mainPage = () => {
-  return <div>테스트</div>;
+  return (
+    <Container>
+      <SideNavbar/>
+      <KeywordPage/>
+    </Container>
+  );
 };
 
 export default mainPage;
