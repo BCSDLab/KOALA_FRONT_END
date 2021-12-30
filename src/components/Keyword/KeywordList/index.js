5d8f8dee82370a882b7736f12b5acf697fa00110
Sangyun <tkfkd4447@naver.com>
2021-12-30 22:04:18 +0900

    [#17] style : 로그인,로그아웃 상태 구현
    user가 현재 로그인을 한 상태라고 가정하고, username+로그아웃 버튼을 구현했습니다.

--- src/components/Keyword/KeywordList/index.js
+++ src/components/Keyword/KeywordList/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const UserContainer = styled.div`
+    position:absolute;
+    right:80px;
+    top:40px;
+`;
+
+const Username = styled.span`
+    line-height:32px;
+    margin-right:16px;
+`;
+
+const LoginButton = styled.button`
+    width: 80px;
+    height: 32px;
+    background: #222;
+    color: #fff;
+    font-size: 14px;
+    font-weight: 500;
+    text-align: center;
+`;
+
 
 const KeywordList = () => {
     return(
         <>
+            <UserContainer>
+                <Username>uko05068님</Username>
+                <LoginButton>로그아웃</LoginButton>
+            </UserContainer>
         </>
     );
 }
--- src/pages/keywordPage.js
+++ src/pages/keywordPage.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import KeywordList from 'components/Keyword/KeywordList';
 
 const KeywordPage = () => {
-  return <div>KeywordPage</div>;
+  return (
+    <>
+      <KeywordList/>
+    </>
+  );
 };
 
 export default KeywordPage;
