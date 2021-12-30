66e2a06240e039414583184f6a3d301514fafd4f
daepan <qw04011@gmail.com>
2021-12-30 16:05:31 +0900

    [#21] refactor : 로그인.로그아웃 함수 useCallback 활용

--- src/components/Shared/LoginButton.js
+++ src/components/Shared/LoginButton.js
@@ -2,6 +2,7 @@ import React, { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { removeCookie } from 'components/Shared/Cookies';
 import { useNavigate } from 'react-router';
+import { useCallback } from 'react';
 
 const LoginButtonStyle = styled.button`
   width: 80px;
@@ -22,15 +23,15 @@ const LoginButton = () => {
   const loginInfo = useSelector((state) => state.auth);
   const navigate = useNavigate();
 
-  const loginClick = () => {
+  const loginClick = useCallback(() => {
     navigate(`/auth`);
-  };
+  });
 
-  const logoutClick = () => {
+  const logoutClick = useCallback(() => {
     removeCookie('refresh_token');
     loginInfo.isLoggedIn = false;
     location.reload();
-  };
+  });
 
   return (
     <div>
