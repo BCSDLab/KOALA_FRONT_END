699e41bd38bfadce4ae29c200c85271f4e078b31
Sangyun <tkfkd4447@naver.com>
2021-12-31 15:09:56 +0900

    [#17] style : 키워드 수정 페이지 큰 뼈대 구성

---
+++ src/components/Keyword/KeywordEdit/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import KeywordHeader from "../KeywordHeader";
+
+const KeywordEdit = () => {
+    return(
+        <>
+            <KeywordHeader/>
+        </>
+    );
+}
+
+export default KeywordEdit;
--- src/pages/keywordPage.js
+++ src/pages/keywordPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import KeywordList from 'components/Keyword/KeywordList';
+import KeywordEdit from 'components/Keyword/KeywordEdit';
 
 const KeywordPage = () => {
   return (
     <>
-      <KeywordList/>
+      <KeywordEdit/>
     </>
   );
 };
