726b6f29e90558ef77a24b3feb21ebb3cfae5649
Sangyun <tkfkd4447@naver.com>
2022-01-01 19:45:13 +0900

    [#17] refactor : Constant 분리

--- src/components/Keyword/KeywordSetting/index.js
+++ src/components/Keyword/KeywordSetting/index.js
@@ -1,17 +1,7 @@
 import React,{Fragment} from "react";
 import KeywordHeader from "../KeywordHeader";
+import { WEEK } from "../constant";
 import * as s from './styles';
-
-const WEEK = [
-    {id:0,day:"월요일"},
-    {id:1,day:"화요일"},
-    {id:2,day:"수요일"},
-    {id:3,day:"목요일"},
-    {id:4,day:"금요일"},
-    {id:5,day:"토요일"},
-    {id:6,day:"일요일"},
-    {id:7,day:"공휴일"},
-];
 
 const KeywordSetting = () => {
     return(
---
+++ src/components/Keyword/constant/index.js
@@ -0,0 +1,10 @@
+export const WEEK = [
+    {id:0,day:"월요일"},
+    {id:1,day:"화요일"},
+    {id:2,day:"수요일"},
+    {id:3,day:"목요일"},
+    {id:4,day:"금요일"},
+    {id:5,day:"토요일"},
+    {id:6,day:"일요일"},
+    {id:7,day:"공휴일"},
+];
