9d6828418d01030d97886ef85509a4150ae1e6ab
KMP <qw03011@naver.com>
2022-01-01 20:35:27 +0900

    [#15] feat : 학교 인증 시 학교이름 표시 기능 추가

--- src/components/Mypage/SchoolAuth.js
+++ src/components/Mypage/SchoolAuth.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 
 const SchoolAuthContent = styled.div`
   display: flex;
 `;
 const SchoolAuthState = styled.div`
-  width: 39px;
+
   height: 21px;
   flex-grow: 0;
   margin: 10px 165px 9px 80px;
@@ -26,11 +27,26 @@ const SchoolAuthButton = styled.button`
   color: #fff;
 `;
 
+/*
+  {
+    TODO:
+    학교인증에 대한 결과로 400일 경우 미인증으로 SchoolAuthState에 표시
+    200 학교명을 띄운고 인증하기 버튼 제거
+  }
+*/
 const SchoolAuth = () => {
+  const AuthState = useSelector((state)=>state.myPage.schoolAuth);
+
   return (
     <SchoolAuthContent>
-      <SchoolAuthState>미인증</SchoolAuthState>
-      <SchoolAuthButton>인증하기</SchoolAuthButton>
+      { AuthState ?
+        <>
+        <SchoolAuthState>미인증</SchoolAuthState>
+        <SchoolAuthButton>인증하기</SchoolAuthButton>
+        </>
+        : <SchoolAuthState>한국기술교육대학교</SchoolAuthState>
+      }
+     
     </SchoolAuthContent>
   );
 };
