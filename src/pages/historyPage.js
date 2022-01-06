80f0b5c593b53e4b38e86c3daa2be0cc8ddd4276
HAEROOL <gofn080776@gmail.com>
2022-01-06 13:58:29 +0900

    feat:히스토리 페이지 추가
    HistoryHeader와 HistoryContent를 사용해 HistoryPage 추가

--- src/pages/historyPage.js
+++ src/pages/historyPage.js
@@ -1,7 +1,23 @@
+
+import HistoryContent from 'components/History/HistoryContent';
+import HistoryHeader from 'components/History/HistoryHeader';
 import React from 'react';
+import styled from 'styled-components';
+const HistoryPageContent = styled.div`
+  padding: 49px 0 0 0;
+  margin: 0 138px;
+  min-width: 1294px;
+  max-width: 1294px;
+`
 
 const HistoryPage = () => {
-  return <div>HistoryPage</div>;
+  return (
+  <HistoryPageContent>
+      <HistoryHeader/>
+      <HistoryContent/>
+  </HistoryPageContent>
+  )
 };
+
 
 export default HistoryPage;
