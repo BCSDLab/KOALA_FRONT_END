fb998cd92aada0aedb1718b09311657e726ab565
Sangyun <tkfkd4447@naver.com>
2022-01-11 10:07:34 +0900

    [#17]  style : ㄹ그인 여부에 따른 헤더 값 변경

--- src/components/Keyword/KeywordHeader/index.js
+++ src/components/Keyword/KeywordHeader/index.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React,{useCallback} from "react";
 import * as s from './styles';
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 
 const KeywordHeader = ({title,toggle}) => {
+
+    const user = useSelector((state)=>state.auth);
+
     return(
         <>
             <s.UserContainer toggle={toggle}>
-                <s.Username>uko05068님</s.Username>
-                <s.LoginButton>로그아웃</s.LoginButton>
+                {user.isLoggedIn&&<s.Username>test</s.Username>}
+                <s.LoginButton onClick={onClickLoggedInButton}>{user.isLoggedIn?'로그아웃':'로그인'}</s.LoginButton>
             </s.UserContainer>
             <s.Title toggle={toggle}>{title}</s.Title>
         </>
