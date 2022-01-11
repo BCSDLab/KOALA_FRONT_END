bffd63dbdc41fa1b7be200c5207db5e4ce8dbb06
Sangyun <tkfkd4447@naver.com>
2022-01-11 10:10:18 +0900

    [#17] refactor : 필요하지 않은 함수 제거

--- src/components/Keyword/KeywordHeader/index.js
+++ src/components/Keyword/KeywordHeader/index.js
@@ -10,7 +10,7 @@ const KeywordHeader = ({title,toggle}) => {
         <>
             <S.UserContainer toggle={toggle}>
                 {user.isLoggedIn&&<S.Username>test</S.Username>}
-                <S.LoginButton onClick={onClickLoggedInButton}>{user.isLoggedIn?'로그아웃':'로그인'}</S.LoginButton>
+                <S.LoginButton>{user.isLoggedIn?'로그아웃':'로그인'}</S.LoginButton>
             </S.UserContainer>
             <S.Title toggle={toggle}>{title}</S.Title>
         </>
