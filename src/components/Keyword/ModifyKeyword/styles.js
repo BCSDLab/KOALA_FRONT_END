fd039dffed96456c3c5df06a6aeb9bb7efeda31d
Sangyun <tkfkd4447@naver.com>
2022-01-15 16:22:26 +0900

    [#20] feat : 중요알림,일반알림, 무음모드알림, 진동알림 기능 구현

--- src/components/Keyword/ModifyKeyword/styles.js
+++ src/components/Keyword/ModifyKeyword/styles.js
@@ -127,10 +127,12 @@ export const ImportantContainer = styled.div`
     left:590px;
     top:501px;
     align-items:center;
+    cursor:pointer;
 `;  
 
 export const NormalContainer = styled(ImportantContainer)`
     top:530px;
+    cursor:pointer;
 `;
 
 export const CheckBox = styled.div`
@@ -139,6 +141,20 @@ export const CheckBox = styled.div`
     margin-right:8px;
     border-radius:3px;
     border:1px solid #c4c4c4;
+    cursor:pointer;
+
+    ${props=>{
+        if(props.isNormalAlarm||props.isImportantAlarm||props.isVibrationAlarm||props.isSlientAlarm){
+            return`
+                background-image:url('/asset/check.svg');
+                background-position:center;
+            `;
+        }else{
+            return`
+                background-image:none;
+            `
+        }
+    }}
 `;
 
 export const CheckBoxTitle = styled.span`
@@ -146,6 +162,7 @@ export const CheckBoxTitle = styled.span`
     font-size:14px;
     color:#999999;
     margin-right:24px;
+    cursor:pointer;
 `;
 
 export const CheckBoxContent = styled.span`
@@ -199,8 +216,15 @@ export const AlarmType = styled.ul`
 `;
 
 export const Type = styled.li`
-    color: #c4c4c4;
+    cursor:pointer;
     margin-right:48px;
+    color: ${props => {
+        if(props.checkId===props.alarmTerm){
+            return `#222222`;
+        }else{
+            return `#c4c4c4`;
+        }
+    }}
 `;
 
 export const EditButton = styled.button`
--- src/constant/index.js
+++ src/constant/index.js
@@ -3,3 +3,34 @@ export const CREATE_ACCOUNT = '/auth/createAccount';
 export const EMAIL_ERROR = '125';
 export const NICKNAME_ERROR = '124';
 export const ACCOUNT_ERROR = '123';
+
+export const ALARM_TERM = [
+    {
+        id:0,
+        time:'5분'
+    },
+    {
+        id:1,
+        time:'10분'
+    },
+    {
+        id:2,
+        time:'15분'
+    },
+    {
+        id:3,
+        time:'30분'
+    },
+    {
+        id:4,
+        time:'1시간'
+    },
+    {
+        id:5,
+        time:'2시간'
+    },
+    {
+        id:6,
+        time:'4시간'
+    },
+]
