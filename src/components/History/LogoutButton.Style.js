2189eddef2fedba48fbed0b26967aa1ed4b2b099
HAEROOL <gofn080776@gmail.com>
2022-01-26 02:00:37 +0900

    [#16] feat:로그아웃버튼 추가

---
+++ src/components/History/LogoutButton.Style.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const UserContainer = styled.div`
+    position:absolute;
+    min-width:173px;
+    left:1664px;
+    top:40px;
+`;
+
+export const Username = styled.span`
+    line-height:32px;
+    margin-right:16px;
+`;
+
+export const LoginButton = styled.button`
+    width: 80px;
+    height: 32px;
+    background: #222;
+    color: #fff;
+    font-size: 14px;
+    font-weight: 500;
+    text-align: center;
+`;
+
+export const Title = styled.span`
+    font-size:18px;
+    min-width:120px;
+    position:absolute;
+    top:121px;
+    left:${props => props.toggle?'488px':'353px'};
+`;
