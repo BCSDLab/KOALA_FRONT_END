f0505e685375d52d770802a2b315a1af11e5b18c
daepan <qw04011@gmail.com>
2022-01-29 15:11:41 +0900

    [#42] fix : current 에러 수정
    Received `false` for a non-boolean attribute `current`. 에 대한 수정

--- src/components/SideNavbar/index.js
+++ src/components/SideNavbar/index.js
@@ -176,37 +176,37 @@ const SideNavbar = () => {
         <MenuButton onClick={toggleSideMenu}>
           <MenuImg src="/asset/MenuBtn.svg" alt="Vector" />
         </MenuButton>
-        <KeywordIcon to="#" current={location.pathname.includes('/keyword')}>
+        <KeywordIcon to="#" current={location.pathname.includes('/keyword') ? 1 : 0}>
           <HashTagImg
             src={location.pathname.includes('/keyword') ? '/asset/Hashtagblack.svg' : '/asset/Hashtag.svg'}
             alt="keyword"
           />
-          {mobile && <MenuItemText current={location.pathname.includes('/keyword')}>키워드</MenuItemText>}
+          {mobile && <MenuItemText current={location.pathname.includes('/keyword') ? 1 : 0}>키워드</MenuItemText>}
         </KeywordIcon>
-        <HistoryIcon to="#" current={location.pathname.includes('/history')}>
+        <HistoryIcon to="#" current={location.pathname.includes('/history') ? 1 : 0}>
           <HistoryImg
             isSideMenu={isOpen}
             src={location.pathname.includes('/history') ? '/asset/HistoryBlack.svg' : '/asset/History.svg'}
             to="#"
             alt="history"
           />
-          {mobile && <MenuItemText current={location.pathname.includes('/history')}>히스토리</MenuItemText>}
+          {mobile && <MenuItemText current={location.pathname.includes('/history') ? 1 : 0}>히스토리</MenuItemText>}
         </HistoryIcon>
-        <ChattingIcon to="#" current={location.pathname.includes('/chat')}>
+        <ChattingIcon to="#" current={location.pathname.includes('/chat') ? 1 : 0}>
           <ChatImg
             isSideMenu={isOpen}
             src={location.pathname.includes('/chat') ? '/asset/Chatblack.svg' : '/asset/Chat.svg'}
             alt="chat"
           />
-          {mobile && <MenuItemText current={location.pathname.includes('/chat')}>채팅방</MenuItemText>}
+          {mobile && <MenuItemText current={location.pathname.includes('/chat') ? 1 : 0}>채팅방</MenuItemText>}
         </ChattingIcon>
-        <SettingIcon to="/mypage" current={location.pathname === '/mypage'}>
+        <SettingIcon to="/mypage" current={location.pathname === '/mypage' ? 1 : 0}>
           <SettingImg
             isSideMenu={isOpen}
             src={location.pathname === '/mypage' ? '/asset/Settingblack.svg' : '/asset/Setting.svg'}
             alt="mypage"
           />
-          {mobile && <MenuItemText current={location.pathname === '/mypage'}>설정</MenuItemText>}
+          {mobile && <MenuItemText current={location.pathname === '/mypage' ? 1 : 0}>설정</MenuItemText>}
         </SettingIcon>
       </Nav>
       {isOpen && <SideNavMenu />}
