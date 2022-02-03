72adcdb4ca0e9e2d9ba63579ef587b6137126789
HAEROOL <gofn080776@gmail.com>
2022-02-03 21:07:06 +0900

    [#16] feat:무한스크롤 기능 추가

--- src/components/History/HistoryContent.js
+++ src/components/History/HistoryContent.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { defaultFallbackInView, useInView } from "react-intersection-observer";
 import HistoryCheckBox from "./HisoryCheckBox";
 import * as S from './History.Style';
 import { getHistoryList, deleteHistoryList, readHistoryItem, moveToScrap} from "store/history";
@@ -16,25 +17,30 @@ const HistoryContent = () => {
     const userInfo = useSelector((state) => state.auth);
     const dispatch = useDispatch();
     const [alertList, setList] = useState([]);
+    const [showList, setShowList] = useState([]);
     const [command, setCommand] = useState(null);
     const [checkedList, setCheckedList] = useState([]);
     const [pageNum, setPageNum] = useState(1);
+    const [isLoading, setLoading] = useState(false);
+    const [refAlert, inView] = useInView({
+        threshold: 0.0,
+        triggerOnce: false,
+      });
+
     const showRead = () =>{
         if(command === 'read'){
             setCommand(null);
-            setList(historyList);
+            setShowList(alertList);
         }else{
             setCommand('read');
-            setList(historyList.filter((mail) => (mail.isRead)));
         }
     }
     const showNotRead = () => {
         if(command === 'notRead'){
             setCommand(null);
-            setList(historyList);
+            setShowList(alertList);
         }else{
             setCommand('notRead');
-            setList(historyList.filter((mail) => (!mail.isRead)));
         }
     }
     const moveToStorage = () => {
@@ -80,19 +86,48 @@ const HistoryContent = () => {
     const clickMail = (id) =>{
         dispatch(readHistoryItem(id));
     }
+
     useEffect(() => {
         if(userInfo.isLoggedIn||deleteHistoryResponse||readHistoryItemResponse||moveToScrapResponse){
+            setLoading(true);
             dispatch(getHistoryList(pageNum));
         }
-    },[userInfo.isLoggedIn,deleteHistoryResponse,readHistoryItemResponse]);
+        setLoading(false);
+    },[userInfo.isLoggedIn,deleteHistoryResponse,readHistoryItemResponse, pageNum]);
+
     useEffect(() => {
-        setList(historyList?.sort((a, b) => {
-            a = stringToDate(a.created_at);
-            b = stringToDate(b.created_at);
-            return a > b ? -1 : a < b ? 1 : 0;
-        }));
-        console.log(historyList);
+        if(historyList.length <= 0){
+            return
+        }else{
+            let sortedHistoryList = historyList?.sort((a, b) => {
+                a = stringToDate(a.created_at);
+                b = stringToDate(b.created_at);
+                return a > b ? -1 : a < b ? 1 : 0;
+            });
+            setList(alertList.concat(sortedHistoryList))
+        }
     }, [historyList]);
+
+    useEffect(() => {
+        switch(command){
+            case 'read':
+                setShowList(alertList.filter((mail) => (mail.isRead)));
+                break;
+            case 'notRead':
+                setShowList(alertList.filter((mail) => (!mail.isRead)));
+                break;
+            case null:
+                setShowList(alertList);
+                break;
+        }
+    },[alertList, command]);
+
+    useEffect(() => {
+        if(inView&&(!isLoading)){
+            setPageNum(pageNum+1);
+        }
+    },[inView, showList])
+
     return (
     <S.PageWrapper>
     <S.MenuList>
@@ -122,14 +157,24 @@ const HistoryContent = () => {
         </S.Menues>
     </S.MenuList>
     <S.KeyWordAlertList>
-            {alertList?.map((mail,id) => (
-                <S.KeyWordAlert isRead = {mail.isRead} key ={id}>
+            {showList?.map((mail,id) => (
+                showList[showList.length-1].id === mail.id?(
+                    <S.KeyWordAlert isRead = {mail.isRead} key ={id} ref={refAlert}>
                     <HistoryCheckBox onClick={(e) => selectMail(e, mail.id)} checked={checkedList.includes(mail.id)?true:false} readOnly/>
                     <S.Sender>{mail.site}</S.Sender>
                     <S.AlertTitle href={mail.url} isRead = {mail.isRead} onClick={() => clickMail(mail.id)}>{mail.title}</S.AlertTitle>
                     <S.MailBrowse>{mail.isRead?'읽음':'읽지않음'}</S.MailBrowse>
                     <S.ReceiveDate>{mail.createdAt}</S.ReceiveDate>
                 </S.KeyWordAlert>
+                ):(
+                    <S.KeyWordAlert isRead = {mail.isRead} key ={id}>
+                    <HistoryCheckBox onClick={(e) => selectMail(e, mail.id)} checked={checkedList.includes(mail.id)?true:false} readOnly/>
+                    <S.Sender>{mail.site}</S.Sender>
+                    <S.AlertTitle href={mail.url} isRead = {mail.isRead} onClick={() => clickMail(mail.id)}>{mail.title}</S.AlertTitle>
+                    <S.MailBrowse>{mail.isRead?'읽음':'읽지않음'}</S.MailBrowse>
+                    <S.ReceiveDate>{mail.createdAt}</S.ReceiveDate>
+                </S.KeyWordAlert>
+                )
             ))}
     </S.KeyWordAlertList>
     </S.PageWrapper>
