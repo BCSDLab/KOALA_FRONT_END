5acc6288b932c4a7a723992af8b5c5840385f584
HAEROOL <gofn080776@gmail.com>
2022-02-19 21:09:49 +0900

    [#16] style:history페이지 헤더 nav 밑줄 로직 변경

--- src/components/History/History/HistoryHeader.js
+++ src/components/History/History/HistoryHeader.js
@@ -9,7 +9,7 @@ const {white, black, darkgray, lightgray, silver, gray, yellow} = theme.colors;
 const Header = styled.div`
   display: flex;
   width: 1294px;
-  padding-bottom: 31px;
+  // padding-bottom: 31px;
   border-bottom: 1px solid #eee;
   @media screen and (max-width: ${theme.deviceSizes.mobileL}) {
     width: 90%;
@@ -35,6 +35,15 @@ const HistoryHeaderTab = styled(NavLink)`
   font-size: 18px;
   margin-right: 72px;
   cursor: pointer;
+  &:after{
+    display: block;
+    content: '';
+    width: 32px;
+    margin: 0 auto;
+    margin-top: 31px;
+    margin-bottom: ${(props) => (props.inlink ? '-2px' : '0')};
+    border-bottom: ${(props) => (props.inlink ? `2px solid ${darkgray}` : '')};
+  }
   @media screen and (max-width: ${theme.deviceSizes.mobileL}) {
     margin: 0 0 ${(props) => (props.inlink ? '-2px' : '0')} 0;
     font-size: 14px;
@@ -64,7 +73,6 @@ const HistoryHeader = ({ location }) => {
           </HistoryHeaderTab>
         </>
       </Header>
-      {!isMobile && <FocusLine location={location} />}
     </>
   );
 };
