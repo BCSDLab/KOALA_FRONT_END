86ea38b8c58a9a5c88f421204de3599e507e4c73
WonBeen Choi <zad1264@naver.com>
2022-04-24 12:56:27 +0000

    fix : 폰트 반복렌더링(깜빡임) 현상 수정

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,45 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'NotoSansCJKKR';
-    font-weight: normal;
-    font-style: normal;
-    src: url(/asset/fonts/NotoSansCJKkr/NotoSansCJKkr-Regular.otf) format('woff');
-  }
-  @font-face {
-    font-family: 'NotoSansCJKKR';
-    font-weight: 500;
-    font-style: normal;
-    src: url(/asset/fonts/NotoSansCJKkr/NotoSansCJKkr-Medium.otf) format('opentype');
-  }
-  @font-face {
-    font-family: 'NotoSansCJKKR';
-    font-weight: bold;
-    font-style: normal;
-    src: url(/asset/fonts/NotoSansCJKkr/NotoSansCJKkr-Bold.otf) format('opentype');
-  }
-  
-  @font-face {
-    font-family: 'NotoSansKR';
-    font-weight: normal; 
-    font-style: normal;
-    src: url(/asset/fonts/NotoSansKR/NotoSansKR-Regular.woff) format('woff');
-  }
-  @font-face {
-      font-family: 'NotoSansKR';
-      font-weight: 500;
-      font-style: normal;
-      src: url(/asset/fonts/NotoSansKR/NotoSansKR-Medium.woff) format('woff');
-  }
-
-  @font-face {
-    font-family: 'NanumSquareRound';
-    font-weight: bold;
-    font-style: normal;
-    src: url(/asset/fonts/other/NanumSquareRoundOTFB.otf) format('opentype');
-}
-
 
   html, body, #root{
     font-family: 'NotoSansCJKKR', 'Noto Sans KR', 'Noto Sans', sans-serif;
